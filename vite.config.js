e9068a1d8427868ef9e1eaf34aa0bb6db820c901
VICTOR DE OLIVEIRA VIEIRA <06937641125@tocantins.governo>
2024-04-22 14:06:12 -0300

    Wip: Add Config Vite

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import reactRefresh from '@vitejs/plugin-react-refresh';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [reactRefresh()],
-  base: "/PortifolioVictor",
+  base: "/PortifolioVictor/",
+//   root: "./src"
 });
